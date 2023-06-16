2687672e35c6ffeb4b91177872568ca7c5ac0cf9
EsraaSolimanMubarak <israamubark856@gmail.com>
2023-06-16 08:10:50 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)  //int main(void) specifies that the main function takes no arguments.
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);    // will always return zero
+}
